f20ca5829b7a938f64a8b8f542c7a0b640bf058d
David Haynes <david.e.haynes@stfc.ac.uk>
2013-10-30 09:01:32 +0000

    Added authentication patching during unittest

--- cloudhands/web/test/test_main.py
+++ cloudhands/web/test/test_main.py
@@ -11,6 +11,21 @@ from cloudhands.web.main import top_page
 
 class TopLevelTests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(class_):
+
+        def testuser_email(req=None):
+            return "testuser@unittest.python"
+
+        class_.auth_unpatch = cloudhands.web.main.authenticated_userid
+        cloudhands.web.main.authenticated_userid = testuser_email
+        if class_.auth_unpatch is cloudhands.web.main.authenticated_userid:
+            class_.skipTest("Authentication badly patched")
+
+    @classmethod
+    def teardownClass(class_):
+        cloudhands.web.main.authenticated_userid = class_.auth_unpatch
+
     def test_version_option(self):
         p = parser()
         rv = p.parse_args(["--version"])
