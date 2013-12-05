a48142cbf496e2f203a66ce4c651d54e6a310579
David Haynes <david.e.haynes@stfc.ac.uk>
2013-12-05 17:43:45 +0000

    Progressively removing tricks

--- cloudhands/web/test/test_tricks.py
+++ cloudhands/web/test/test_tricks.py
@@ -13,75 +13,14 @@ from cloudhands.common.fsm import HostState
 from cloudhands.common.fsm import MembershipState
 
 import cloudhands.common.schema
-from cloudhands.common.schema import EmailAddress
 from cloudhands.common.schema import Host
 from cloudhands.common.schema import IPAddress
-from cloudhands.common.schema import Membership
 from cloudhands.common.schema import Organisation
 from cloudhands.common.schema import State
 from cloudhands.common.schema import Touch
 from cloudhands.common.schema import User
 
 from cloudhands.web.tricks import allocate_ip
-from cloudhands.web.tricks import create_user_from_email
-from cloudhands.web.tricks import handle_from_email
-
-
-class TestUserMembership(unittest.TestCase):
-
-    def setUp(self):
-        """ Populate test database"""
-        session = Registry().connect(sqlite3, ":memory:").session
-        session.add_all(
-            State(fsm=MembershipState.table, name=v)
-            for v in MembershipState.values)
-        session.add(Organisation(name="TestOrg"))
-        session.commit()
-
-    def tearDown(self):
-        """ Every test gets its own in-memory database """
-        r = Registry()
-        r.disconnect(sqlite3, ":memory:")
-
-    def test_quick_add_user(self):
-        session = Registry().connect(sqlite3, ":memory:").session
-        oName = "TestOrg"
-        eAddr = "my.name@test.org"
-
-        org = session.query(
-            Organisation).filter(Organisation.name == oName).one()
-        invitation = Membership(
-            uuid=uuid.uuid4().hex,
-            model=cloudhands.common.__version__,
-            organisation=org,
-            role="user")
-        handle = handle_from_email(eAddr)
-        user = create_user_from_email(session, eAddr, handle, invitation)
-        self.assertIs(user, session.query(User).join(Touch).join(
-            EmailAddress).filter(EmailAddress.value == eAddr).first())
-
-    def test_add_duplicate_user(self):
-        session = Registry().connect(sqlite3, ":memory:").session
-        session.autoflush = False   # http://stackoverflow.com/a/4202016
-        oName = "TestOrg"
-        eAddr = "my.name@test.org"
-
-        org = session.query(
-            Organisation).filter(Organisation.name == oName).one()
-        invitation = Membership(
-            uuid=uuid.uuid4().hex,
-            model=cloudhands.common.__version__,
-            organisation=org,
-            role="user")
-        handle = handle_from_email(eAddr)
-        user = create_user_from_email(session, eAddr, handle, invitation)
-        reInvitation = Membership(
-            uuid=uuid.uuid4().hex,
-            model=cloudhands.common.__version__,
-            organisation=org,
-            role="user")
-        self.assertIsNone(create_user_from_email(
-            session, eAddr, handle, reInvitation))
 
 
 class TestResourceManagement(unittest.TestCase):
@@ -107,19 +46,14 @@ class TestResourceManagement(unittest.TestCase):
         session = Registry().connect(sqlite3, ":memory:").session
         session.autoflush = False   # http://stackoverflow.com/a/4202016
         oName = "TestOrg"
-        eAddr = "my.name@test.org"
+        handle = "Test User"
         hName = "mynode.test.org"
         ipAddr = "192.168.1.1"
 
-        org = session.query(
-            Organisation).filter(Organisation.name == oName).one()
-        invitation = Membership(
-            uuid=uuid.uuid4().hex,
-            model=cloudhands.common.__version__,
-            organisation=org,
-            role="user")
-        handle = handle_from_email(eAddr)
-        user = create_user_from_email(session, eAddr, handle, invitation)
+        user = User(handle=handle, uuid=uuid.uuid4().hex)
+        org = session.query(Organisation).one()
+        session.add_all((user, org))
+        session.commit()
 
         scheduling = session.query(HostState).filter(
             HostState.name == "scheduling").one()
--- cloudhands/web/tricks.py
+++ cloudhands/web/tricks.py
@@ -21,52 +21,6 @@ __doc__ = """
 Common functions for interacting with the schema.
 """
 
-
-def handle_from_email(addrVal):
-    return ' '.join(
-        i.capitalize() for i in addrVal.split('@')[0].split('.'))
-
-
-def create_user_from_email(session, addrVal, handle, invitation):
-    """
-    Creates a new user account from an email address and an invitation.
-    The sequence of operations is:
-
-    1.  Create a User record.
-    2.  Create an EmailAddress resource
-    3.  Touch the invitation with the user, the email, and a state of `active`.
-
-    :param object session:  A SQLALchemy database session.
-    :param string addrVal:   The user's email address.
-    :param string handle:   Becomes the user's handle. If not supplied,
-                            handle is constructed from the `addrVal`.
-    :param object invitation: A Membership object.
-    :returns: The newly created User object.
-    """
-    log = logging.getLogger("cloudhands.common.tricks")
-
-    user = User(handle=handle, uuid=uuid.uuid4().hex)
-    try:
-        session.add(user)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        log.warning("User with handle '{}' exists".format(handle))
-        return None
-
-    active = session.query(
-        MembershipState).filter(MembershipState.name == "active").one()
-    now = datetime.datetime.utcnow()
-
-    activation = Touch(artifact=invitation, actor=user, state=active, at=now)
-    invitation.changes.append(activation)
-    ea = EmailAddress(
-        value=addrVal, touch=activation, provider=addrVal.split('@')[1])
-    session.add(ea)
-    session.commit()
-    return user
-
-
 def allocate_ip(session, host, ipAddr, provider="unknown"):
     session.query(IPAddress).filter(IPAddress.value == ipAddr).delete()
 
