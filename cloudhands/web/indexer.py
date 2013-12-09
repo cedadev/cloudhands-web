591d8b992801c928691b59b623e2dab3b0088813
David Haynes <david.e.haynes@stfc.ac.uk>
2013-12-09 16:50:07 +0000

    Indexer gets settings by discovery

--- cloudhands/web/indexer.py
+++ cloudhands/web/indexer.py
@@ -15,6 +15,8 @@ import whoosh.qparser
 import whoosh.query
 import whoosh.writing
 
+from cloudhands.common.discovery import settings
+
 __doc__ = """
 ldapsearch -x -H ldap://homer.esc.rl.ac.uk -s sub -b
 'ou=ceda,ou=People,o=hpc,dc=rl,dc=ac,dc=uk'
@@ -22,21 +24,6 @@ ldapsearch -x -H ldap://homer.esc.rl.ac.uk -s sub -b
 """
 
 DFLT_IX = "cloudhands.wsh"
-
-ldap_search = {
-    "host": "homer.esc.rl.ac.uk",
-    "port": "389",
-    "query": "ou=ceda,ou=People,o=hpc,dc=rl,dc=ac,dc=uk",
-    "filter": "(&(objectclass=posixAccount)(objectclass=ldapPublicKey))",
-}
-ldap_attributes = {
-    "cn": True,
-    "gecos": True,
-    "uid": True,
-    "uidNumber": True,
-    "gidNumber": True,
-    "sshPublicKey": True,
-}
 
 ldap_types = {
     "cn": whoosh.fields.ID(stored=True),
@@ -48,25 +35,27 @@ ldap_types = {
 }
 
 
-def index(args, loop=None):
+def index(args, config, loop=None):
     log = logging.getLogger("cloudhands.web.indexer")
     schema = whoosh.fields.Schema(dn=whoosh.fields.ID(),
-        **{k: v for k, v in ldap_types.items() if ldap_attributes[k]})
+        **{k: v for k, v in ldap_types.items()
+        if config.getboolean("ldap.attributes", k)})
 
     ix = whoosh.index.create_in(args.index, schema=schema)
     ix = whoosh.index.open_dir(args.index)
 
     s = ldap3.server.Server(
-        ldap_search["host"],
-        port=int(ldap_search["port"]),
+        config["ldap.search"]["host"],
+        port=int(config["ldap.search"]["port"]),
         getInfo=ldap3.GET_ALL_INFO)
     c = ldap3.connection.Connection(
         s, autoBind=True, clientStrategy=ldap3.STRATEGY_SYNC)
 
     search = functools.partial(
-        c.search, ldap_search ["query"], ldap_search["filter"],
+        c.search,
+        config["ldap.search"]["query"], config["ldap.search"]["filter"],
         ldap3.SEARCH_SCOPE_WHOLE_SUBTREE,
-        attributes=[k for k, v in ldap_attributes.items() if v])
+        attributes=[k for k, v in config["ldap.attributes"].items() if v])
 
     def pager(size=128):
         cookie = True
@@ -88,7 +77,7 @@ def index(args, loop=None):
     c.unbind()
     if loop is not None:
         log.debug("Rescheduling {}s later".format(args.interval))
-        loop.enter(args.interval, 0, index, (args, loop))
+        loop.enter(args.interval, 0, index, (args, config, loop))
     return n
 
 
@@ -96,6 +85,7 @@ def main(args):
     logging.basicConfig(
         level=args.log_level,
         format="%(asctime)s %(levelname)-7s %(name)s|%(message)s")
+    provider, config = next(iter(settings.items()))
     loop = sched.scheduler()
 
     try:
@@ -121,9 +111,9 @@ def main(args):
         return 0
 
     if args.interval is None:
-        return index(args) > 0
+        return index(args, config) > 0
     else:
-        loop.enter(args.interval, 0, index, (args, loop))
+        loop.enter(args.interval, 0, index, (args, config, loop))
         loop.run()
         return 1
 
