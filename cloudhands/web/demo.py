8001dd082aa9f0c26131d5ff2f10748162e1a0e2
David Haynes <david.e.haynes@stfc.ac.uk>
2014-02-06 15:50:19 +0000

    web demo allocates subscribed resources

--- cloudhands/web/demo.py
+++ cloudhands/web/demo.py
@@ -18,6 +18,7 @@ from cloudhands.common.discovery import settings
 from cloudhands.common.fsm import MembershipState
 
 from cloudhands.common.schema import Archive
+from cloudhands.common.schema import Directory
 from cloudhands.common.schema import EmailAddress
 from cloudhands.common.schema import Membership
 from cloudhands.common.schema import Organisation
@@ -67,10 +68,13 @@ class WebFixture(object):
                 session.commit()
             except Exception as e:
                 session.rollback()
+            finally:
+                session.flush()
 
     def grant_admin_memberships(session, user):
         active = session.query(MembershipState).filter(
             MembershipState.name == "active").one()
+
         for name, provider in WebFixture.organisations:
             org = session.query(Organisation).filter(
                 Organisation.name == name).one()
@@ -92,6 +96,37 @@ class WebFixture(object):
                 yield act
             except Exception as e:
                 session.rollback()
+            finally:
+                session.flush()
+
+
+    def add_subscribed_resources(session, user):
+        for name, provider in WebFixture.organisations:
+            org = session.query(Organisation).filter(
+                Organisation.name == name).one()
+            grant = session.query(Touch).join(Membership).join(User).filter(
+                Membership.organisation_id == org.id).filter(
+                User.id == user.id).first()
+            mship = grant.artifact
+            for subs in org.subscriptions:
+                now = datetime.datetime.utcnow()
+                if isinstance(subs.provider, Archive):
+                    d = Directory(
+                        provider=subs.provider,
+                        description="CIDER data archive",
+                        mount_path="/{mount}/panfs/cider")
+                    act = Touch(
+                        artifact=mship, actor=user, state=grant.state, at=now)
+                    d.touch = act
+                    mship.changes.append(act)
+                    try:
+                        session.add_all((d, act))
+                        session.commit()
+                        yield act
+                    except Exception as e:
+                        session.rollback()
+                    finally:
+                        session.flush()
 
 
 def main(args):
@@ -115,6 +150,15 @@ def main(args):
         log.info("{} activated as admin for {}".format(
             t.actor.handle,
             t.artifact.organisation.name))
+
+    user = session.query(User).filter(User.handle == user.handle).one()
+    for t in WebFixture.add_subscribed_resources(session, user):
+        for r in t.resources:
+            log.info("{} permitted access to a {} resource ({}).".format(
+                t.actor.handle,
+                r.provider.name,
+                getattr(r, "description", "?")))
+
 
     cloudhands.web.main.authenticated_userid = WebFixture.demo_email
 
