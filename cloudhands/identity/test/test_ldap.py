c81874e22d76d64662f66249e9f9bf2a1be504a6
David Haynes <david.e.haynes@stfc.ac.uk>
2014-04-17 16:13:22 +0100

    Added reference strings for LDAP

--- cloudhands/identity/test/test_ldap.py
+++ cloudhands/identity/test/test_ldap.py
@@ -11,15 +11,18 @@ class LDAPRecordTests(unittest.TestCase):
     objectclass: top
     objectclass: person
     objectclass: organizationalPerson
+    objectclass: inetOrgPerson
     objectclass: posixAccount
     objectclass: ldapPublicKey
     description: JASMIN2 vCloud account
     userPassword: {SHA}0LXhFAsrBWEEQ
     cn: dehaynes
     sn: Haynes
+    ou: jasmin2
     uid: dehaynes
     uidNumber: 1034
     gidNumber: 100
+    mail: david.e.haynes@stfc.ac.uk
     homeDirectory: /home/dehaynes
     sshPublicKey: ssh-dss AAAAB3...
     sshPublicKey: ssh-dss AAAAM5...
@@ -30,6 +33,7 @@ class LDAPRecordTests(unittest.TestCase):
         dn: cn=3dceb7f3dc9947b78345f864972ee31f,ou=jasmin2,ou=People,o=hpc,dc=rl,dc=ac,dc=uk
         objectclass: top
         objectclass: person
+        description: JASMIN2 vCloud membership
         cn: 3dceb7f3dc9947b78345f864972ee31f
         sn: UNKNOWN
         """
@@ -37,20 +41,85 @@ class LDAPRecordTests(unittest.TestCase):
 
     def test_state_two(self):
         expect = """
+        dn: cn=3dceb7f3dc9947b78345f864972ee31f,ou=jasmin2,ou=People,o=hpc,dc=rl,dc=ac,dc=uk
+        objectclass: top
+        objectclass: person
+        objectclass: organizationalPerson
+        objectclass: inetOrgPerson
+        description: JASMIN2 vCloud membership
+        cn: 3dceb7f3dc9947b78345f864972ee31f
+        sn: UNKNOWN
+        ou: jasmin2
+        mail: david.e.haynes@stfc.ac.uk
+        """
+
+    def test_state_three(self):
+        expect = """
+        dn: cn=3dceb7f3dc9947b78345f864972ee31f,ou=jasmin2,ou=People,o=hpc,dc=rl,dc=ac,dc=uk
+        objectclass: top
+        objectclass: person
+        objectclass: organizationalPerson
+        objectclass: inetOrgPerson
+        description: JASMIN2 vCloud membership
+        cn: 3dceb7f3dc9947b78345f864972ee31f
+        sn: Haynes
+        ou: jasmin2
+        mail: david.e.haynes@stfc.ac.uk
+        """
+
+    def test_state_four(self):
+        expect = """
         dn: cn=dehaynes,ou=jasmin2,ou=People,o=hpc,dc=rl,dc=ac,dc=uk
         objectclass: top
         objectclass: person
         objectclass: organizationalPerson
+        objectclass: inetOrgPerson
+        description: JASMIN2 vCloud user
+        cn: dehaynes
+        sn: Haynes
+        ou: jasmin2
+        mail: david.e.haynes@stfc.ac.uk
+        """
+
+    def test_state_five(self):
+        expect = """
+        dn: cn=dehaynes,ou=jasmin2,ou=People,o=hpc,dc=rl,dc=ac,dc=uk
+        objectclass: top
+        objectclass: person
+        objectclass: organizationalPerson
+        objectclass: inetOrgPerson
+        objectclass: posixAccount
+        description: JASMIN2 vCloud account
+        userPassword: {SHA}0LXhFAsrBWEEQ
+        cn: dehaynes
+        sn: Haynes
+        ou: jasmin2
+        uid: dehaynes
+        uidNumber: 1034
+        gidNumber: 100
+        mail: david.e.haynes@stfc.ac.uk
+        homeDirectory: /home/dehaynes
+        """
+
+    def test_state_six(self):
+        expect = """
+        dn: cn=dehaynes,ou=jasmin2,ou=People,o=hpc,dc=rl,dc=ac,dc=uk
+        objectclass: top
+        objectclass: person
+        objectclass: organizationalPerson
+        objectclass: inetOrgPerson
         objectclass: posixAccount
         objectclass: ldapPublicKey
         description: JASMIN2 vCloud account
         userPassword: {SHA}0LXhFAsrBWEEQ
         cn: dehaynes
         sn: Haynes
+        ou: jasmin2
         uid: dehaynes
         uidNumber: 1034
         gidNumber: 100
+        mail: david.e.haynes@stfc.ac.uk
         homeDirectory: /home/dehaynes
         sshPublicKey: ssh-dss AAAAB3...
-        sshPublicKey: ssh-dss AAAAM5...
         """
+
