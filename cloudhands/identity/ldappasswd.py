bf4a3ee2a561c11fbb65de8dbf11d95944d3e1ff
David Haynes <david.e.haynes@stfc.ac.uk>
2014-06-04 15:02:17 +0100

    Minor

--- cloudhands/identity/ldappasswd.py
+++ cloudhands/identity/ldappasswd.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 # encoding: UTF-8
 
+import argparse
+import logging
+import subprocess
+import sys
+
+from cloudhands.web import __version__
+
 __doc__ = """
 Command line tool to change LDAP passwords.
 """
 
+"""
+ ldappasswd -D " cn=Nick
+Aurelius-Haddock,ou=jasmin,ou=People,o=hpc,dc=rl,dc=ac,dc=uk " -w
+"<your_password>" -H ldap://ldap-test.jc.rl.ac.uk  -A -S
+"cn=nahaddock,ou=ceda,ou=People,o=hpc,dc=rl,dc=ac,dc=uk"
+"""
+
+def main(args):
+    rv = subprocess.call(["ldappasswd", "-h"])
+    return rv
+
+    
 def parser(descr=__doc__):
     rv = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=descr)
-    rv.add_argument(
-        "--name", default=None,
-        help="Print a new LDAP record with the given name")
-    rv.add_argument(
-        "--db", default=DFLT_DB,
-        help="Set the path to the database [{}]".format(DFLT_DB))
     rv.add_argument(
         "--version", action="store_true", default=False,
         help="Print the current version number")
@@ -32,15 +45,6 @@ def run():
     rv = 0
     if args.version:
         sys.stdout.write(__version__ + "\n")
-    if args.name:
-        sys.stdout.write(textwrap.dedent("""
-        dn: cn={0},ou=jasmin2,ou=People,o=hpc,dc=rl,dc=ac,dc=uk
-        objectclass: top
-        objectclass: person
-        description: JASMIN2 vCloud registration
-        cn: {0}
-        sn: UNKNOWN
-        """.format(args.name)))
     else:
         rv = main(args)
     sys.exit(rv)
