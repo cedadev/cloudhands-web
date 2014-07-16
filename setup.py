dcabf9b6e45189e598a6ac1da6525f20ffbfc54c
David Haynes <david.e.haynes@stfc.ac.uk>
2014-07-16 08:53:47 +0100

    Corrected pinning and url.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description="Web portal for cloudhands PaaS",
     author="D Haynes",
     author_email="david.e.haynes@stfc.ac.uk",
-    url="http://pypi.python.org/pypi/cloudhands-web",
+    url="https://github.com/cedadev/cloudhands-web.git",
     long_description=__doc__,
     classifiers=[
         "Operating System :: OS Independent",
@@ -51,7 +51,7 @@ setup(
         },
     install_requires=[
         "asyncio>=0.4.1",
-        "cloudhands-common>=0.23",
+        "cloudhands-common>=0.24",
         "singledispatch>=3.4.0.2",
         "pyramid>=1.4.0",
         "pyramid_authstack>=1.0.1",
