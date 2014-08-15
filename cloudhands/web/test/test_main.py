175faf5dcad57be4e7099ec13e75293b56636785
David Haynes <david.e.haynes@stfc.ac.uk>
2014-08-15 11:34:01 +0100

    Removed placeholder unit test.

--- cloudhands/web/test/test_main.py
+++ cloudhands/web/test/test_main.py
@@ -437,11 +437,6 @@ class LoginAndOutTests(ServerTests):
             "pre_user_ldappublickey",
             reg.changes[-1].state.name)
 
-    def test_memberships_get_session_token_to_each_organisation_provider(self):
-        self.fail(
-            "Need to create a session resource for every provider "
-            "and attach to a Membership artifact. "
-            "Not here though. Goes in cloudhands-common.")
 
 class MembershipPageTests(ServerTests):
 
